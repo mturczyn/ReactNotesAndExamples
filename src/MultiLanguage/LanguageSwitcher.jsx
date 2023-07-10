c14b98d81d15e15eed5729da7a30865971961fdc
Michal Turczyn <turek1992@o2.pl>
2023-07-10 14:59:38 +0200

    Added example of multilanguage components.

---
+++ src/MultiLanguage/LanguageSwitcher.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const LanguageSwitcher = () => {
+  const { i18n } = useTranslation();
+
+  const handleLanguageChange = (e) => {
+    const newLang = e.target.value;
+    i18n.changeLanguage(newLang);
+  };
+
+  return (
+    <select value={i18n.language} onChange={handleLanguageChange}>
+      <option value="en">English</option>
+      <option value="fr">Français</option>
+      <option value="es">Español</option>
+    </select>
+  );
+};
+
+export default LanguageSwitcher;
