a2d0ad5f9ed43fe402159b44204368ac2be9a357
Michal Turczyn <turek1992@o2.pl>
2024-02-22 14:54:16 +0100

    Added OpenLayers map.

--- src/Maps/index.js
+++ src/Maps/index.js
@@ -1,3 +1,40 @@
+import { Map, View } from 'ol'
+import OSM from 'ol/source/OSM'
+import TileLayer from 'ol/layer/Tile'
+import { useEffect } from 'react'
+import 'ol/ol.css' // Import OpenLayers CSS
+
 export default function Maps() {
-    return <h1>Maps</h1>
+    useEffect(() => {
+        const osmLayer = new TileLayer({
+            preload: Infinity,
+            source: new OSM(),
+        })
+        const map = new Map({
+            // The ID of the element that will contain the map.
+            target: 'map-container',
+            layers: [osmLayer],
+            view: new View({
+                center: [0, 0],
+                zoom: 0,
+            }),
+        })
+        return () => map.setTarget(null)
+    }, [])
+
+    return (
+        <>
+            <h1>Maps</h1>
+            <div
+                id="map-container"
+                className="map-container"
+                style={{
+                    width: '100%',
+                    height: '500px',
+                    border: '1px solid black',
+                    margin: '1ch',
+                }}
+            ></div>
+        </>
+    )
 }
