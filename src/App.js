3cba74457c8982d073ceabfb33348bafee606ac9
Michal Turczyn <turek1992@o2.pl>
2024-02-22 14:04:56 +0100

    Added router.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import WeatherApp from './MultiLanguage/WeatherApp';
+import logo from './logo.svg'
+import './App.css'
+import WeatherApp from './MultiLanguage/WeatherApp'
+import { Link } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <WeatherApp />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <Link to="/maps">Go to maps</Link>
+                <WeatherApp />
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/MultiLanguage/WeatherApp.jsx
+++ src/MultiLanguage/WeatherApp.jsx
@@ -1,73 +1,75 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useTranslation } from "react-i18next";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import translationEN from "./locales/en/translation.json";
-import translationFR from "./locales/fr/translation.json";
-import translationES from "./locales/es/translation.json";
-import LanguageSwitcher from "./LanguageSwitcher";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useTranslation } from 'react-i18next'
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import translationEN from './locales/en/translation.json'
+import translationFR from './locales/fr/translation.json'
+import translationES from './locales/es/translation.json'
+import LanguageSwitcher from './LanguageSwitcher'
 
 const resources = {
-  en: {
-    translation: translationEN,
-  },
-  fr: {
-    translation: translationFR,
-  },
-  es: {
-    translation: translationES,
-  },
-};
+    en: {
+        translation: translationEN,
+    },
+    fr: {
+        translation: translationFR,
+    },
+    es: {
+        translation: translationES,
+    },
+}
 
 i18n.use(initReactI18next).init({
-  resources,
-  lng: "en",
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+        escapeValue: false,
+    },
+})
 
 const WeatherApp = () => {
-  const { t } = useTranslation();
-  const [city, setCity] = useState("");
-  const [weatherData, setWeatherData] = useState({});
+    const { t } = useTranslation()
+    const [city, setCity] = useState('')
+    const [weatherData, setWeatherData] = useState({})
 
-  useEffect(() => {
-    const getWeatherData = async () => {
-      const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9ba6ce0196b42b189d5a8a33aaa888f9`
-      );
-      setWeatherData(res.data);
-    };
-    getWeatherData();
-  }, [city]);
+    useEffect(() => {
+        ;(async () => {
+            try {
+                const res = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9ba6ce0196b42b189d5a8a33aaa888f9`
+                )
+                setWeatherData(res.data)
+            } catch (error) {
+                console.log(error)
+            }
+        })()
+    }, [city])
 
-  const handleCityChange = async (e) => {
-    const newCity = e.target.value;
-    setCity(newCity);
+    const handleCityChange = async (e) => {
+        const newCity = e.target.value
+        setCity(newCity)
+    }
+    console.log(weatherData)
+    return (
+        <div>
+            <LanguageSwitcher />
+            <input type="text" value={city} onChange={handleCityChange} />
+            <h2>
+                {t('weather_forcast_for')}: {weatherData.name}
+            </h2>
+            <p>
+                {t('current_temperature')}: {weatherData.main?.temp}
+            </p>
+            <p>
+                {t('feels_like')}: {weatherData.main?.feels_like}
+            </p>
+            <p>
+                {t('humidity')}: {weatherData.main?.humidity}
+            </p>
+        </div>
+    )
+}
 
-  };
-  console.log(weatherData)
-  return (
-   <div>
-    <LanguageSwitcher />
-    <input type="text" value={city} onChange={handleCityChange} />
-    <h2>
-      {t("weather_forcast_for")}: {weatherData.name}
-    </h2>
-    <p>
-      {t("current_temperature")}: {weatherData.main?.temp}
-    </p>
-    <p>
-      {t("feels_like")}: {weatherData.main?.feels_like}
-    </p>
-    <p>
-      {t("humidity")}: {weatherData.main?.humidity}
-    </p>
-  </div>
-  );
-};
-
-export default WeatherApp;
+export default WeatherApp
